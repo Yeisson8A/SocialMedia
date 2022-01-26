d1a890b8d69ff13e431cbc4be6905f91b9358b53
Yeisson Ochoa <yeisson.ochoa72@gmail.com>
2022-01-26 16:57:38 -0500

    Subiendo proyecto

---
+++ SocialMedia.Api/Controllers/PostController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        //Método GET
+        [HttpGet]
+        public IActionResult GetPosts()
+        {
+            var posts = new PostRepository().GetPosts();
+            return Ok(posts);
+        }
+    }
+}
---
+++ SocialMedia.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,25 @@
+﻿using SocialMedia.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SocialMedia.Infrastructure.Repositories
+{
+    public class PostRepository
+    {
+        public IEnumerable<Post> GetPosts()
+        {
+            var posts = Enumerable.Range(1, 10).Select(x => new Post
+            {
+                PostId = x,
+                UserId = x * 2,
+                Date = DateTime.Now,
+                Description = $"Description {x}",
+                Image = ""
+            });
+
+            return posts;
+        }
+    }
+}
